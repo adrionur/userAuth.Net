85ce640cab9998d873d1f818fd2310de57671cf5
Onur Koç <onur.a.koc@ericsson.com>
2020-06-07 19:56:47 +0300

    Test Commit

---
+++ LoginApp.Web/Controllers/AccountController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LoginApp.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Login()
+        {
+            if (Request.IsAuthenticated)
+                return View("Home", "Index");
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login()
+        {
+            return View();
+        }
+    }
+}
